9e3d8b0046a5bc3238701c7b9b3f717470a9daac
batwood99 <atwood@mail.usf.edu>
2023-03-24 10:45:47 -0400

    Corrected function to save text area data to local console and display upon loading page.

--- Develop/script.js
+++ Develop/script.js
@@ -1,50 +1,24 @@
 var today = dayjs();
 $('#currentDay').text(today.format('dddd MMM D, YYYY'));
 
-var h9FormEl = $('#hour-9-container');
-var h10FormEl = $('#hour-10-container');
-var h11FormEl = $('#hour-11-container');
-var h12FormEl = $('#hour-12-container');
-var h13FormEl = $('#hour-13-container');
-var h14FormEl = $('#hour-14-container');
-var h15FormEl = $('#hour-15-container');
-var h16FormEl = $('#hour-16-container');
-var h17FormEl = $('#hour-17-container');
-var h9TaskEl = $('#hour-9-text');
-var h10TaskEl = $('#hour-10-text');
-var h11TaskEl = $('#hour-11-text');
-var h12TaskEl = $('#hour-12-text');
-var h13TaskEl = $('#hour-13-text');
-var h14TaskEl = $('#hour-14-text');
-var h15TaskEl = $('#hour-15-text');
-var h16TaskEl = $('#hour-16-text');
-var h17TaskEl = $('#hour-17-text');
+$(document).ready(function() {
 
-function readH9TasksFromStorage() {
-    var H9Tasks = localStorage.getItem('H9Tasks')
-    if (H9Tasks) {
-        H9Tasks = JSON.parse(H9Tasks);
-    } else {
-        H9Tasks = [];
-    }
-    return H9Tasks;
-}
+    $(".saveBtn").on("click", function() {
 
-function saveH9TasksToStorage(H9Tasks) {
-    localStorage.setItem('H9Tasks', JSON.stringify(H9Tasks));
-}
+        var text = $(this).siblings(".description").val();
+        var hour = $(this).parent().attr("id");
 
-function printH9TaskData() {
-    H9TasksDisplayEl.empty();
+        localStorage.setItem(hour, text);
+    });
 
+    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+    $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+    $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+    $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+    $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+    $("#hour-17 .description").val(localStorage.getItem("hour-17"));
 
-var H9Tasks = readH9TasksFromStorage();
-
-for (var i = 0; i < H9Tasks.length; i += 1) {
-    var H9Tasks = H9Tasks[i];
-
-    var rowEl = $('<textarea');
-
-}
-
-}
+});
